2e35374aa35ed175258af7beac1b3f1b23cbaa74
r33an1 <r33an1@me.com>
2018-11-11 17:20:30 +0100

    coordinate changes for gmaps

--- www/garvergarden/map.js
+++ www/garvergarden/map.js
@@ -1,6 +1,6 @@
 function myMap() {
     var mapProp= {
-        center: new google.maps.LatLng(60.883108, 11.560936),
+        center: new google.maps.LatLng(60.883048, 11.560024),
         zoom: 17,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
--- www/gimle/map.js
+++ www/gimle/map.js
@@ -1,6 +1,6 @@
 function myMap() {
     var mapProp= {
-        center: new google.maps.LatLng(60.883108, 11.560936),
+        center: new google.maps.LatLng(60.883605, 11.560679),
         zoom: 17,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
